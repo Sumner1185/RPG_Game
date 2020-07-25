ced1805df99640465999e38486b8c1dbffea13ab
Sumner1185 <alastair.sumner@yahoo.com>
2020-07-25 13:23:16 +0100

    Add tile images and create tile matrix

--- src/features/map/index.js
+++ src/features/map/index.js
@@ -1,5 +1,15 @@
 import React from 'react'
 
+function MapTile(props) {
+  return <div >
+    0
+  </div>
+}
+
+function MapRow(props) {
+  return props.tiles.map( tile => <MapTile value={tile} />)
+}
+
 function Map(props) {
   return (
     <div
@@ -12,7 +22,11 @@ function Map(props) {
       backgroundColor: 'green',
       border: '4px solid white',
     }}
-    />
+    >
+      {
+        props.tiles.map( row => <MapRow tiles={row} /> )
+      }
+    </div>
   )
 }
 

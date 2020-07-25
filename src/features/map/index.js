800c2541ea251cdd59ac0a30c80ef23a3e3bac3b
Sumner1185 <alastair.sumner@yahoo.com>
2020-07-25 14:04:23 +0100

    Saved tiles in reducer

--- src/features/map/index.js
+++ src/features/map/index.js
@@ -1,13 +1,38 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { SPRITE_SIZE } from '../../config/constants'
+
+import './styles.css'
+
+function getTileSprite(type) {
+  switch(type) {
+    case 0:
+      return 'grass'
+    case 5:
+      return 'rock'
+    case 6:
+      return 'tree'
+    default:
+      console.log("No tiles")
+  }
+}
 
 function MapTile(props) {
-  return <div >
-    0
-  </div>
+  return <div 
+    className={`tile ${getTileSprite(props.tile)}`}
+    style={{
+      height: SPRITE_SIZE,
+      width: SPRITE_SIZE,
+    }}
+  />
 }
 
 function MapRow(props) {
-  return props.tiles.map( tile => <MapTile value={tile} />)
+  return <div className="row">
+    {
+      props.tiles.map( tile => <MapTile tile={tile} /> )
+    }
+  </div>
 }
 
 function Map(props) {
@@ -30,4 +55,10 @@ function Map(props) {
   )
 }
 
-export default Map
+function mapStateToProps(state) {
+  return {
+    tiles: state.map.tiles
+  }
+}
+
+export default connect(mapStateToProps)(Map)
